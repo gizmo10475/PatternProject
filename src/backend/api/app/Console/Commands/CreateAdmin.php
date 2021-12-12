6d29f7ff5911556afae0cd2481de5d34f9386c5c
Wilhelm Gren <wilhelmgren@outlook.com>
2021-12-12 20:32:28 +0100

    Command for creating administrator

---
+++ src/backend/api/app/Console/Commands/CreateAdmin.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Admin;
+
+class CreateAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = "admin:create {email}";
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Create an administrator";
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $email = $this->argument("email");
+        $mightExist = Admin::query()->firstWhere("email", "=", $email);
+        if ($mightExist) {
+            $this->error("Email already registered.");
+            return;
+        }
+        $account = Admin::create(["email" => $email]);
+        $token = $account->createToken("pattern", ["pattern:admin"])->plainTextToken;
+        echo "Administrator account for $email created. Your access token is $token\n";
+        return Command::SUCCESS;
+    }
+}

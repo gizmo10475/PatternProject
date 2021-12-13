b3d158b704589c8598c4492a7196d45dba22ea79
Sigrid <sigge.jonsson@hotmail.se>
2021-12-13 14:51:52 +0100

    Implemented OAuth

--- src/frontend/webclient/index.js
+++ src/frontend/webclient/index.js
@@ -1,6 +1,4 @@
-
 "use strict";
-
 const port = process.env.DBWEBB_PORT || 1337;
 const path = require("path");
 const express = require("express");
@@ -9,11 +7,75 @@ const routeIndex = require("./route/index.js");
 const routeCustomer = require("./route/customer.js");
 const routeAdmin = require("./route/admin.js");
 const middleware = require("./middleware/index.js");
+const config = require("./config/config.json");
+
 
 app.set("view engine", "ejs");
 
 app.use(middleware.logIncomingToConsole);
 app.use(express.static(path.join(__dirname, "public")));
+
+const clientId = config.clientId;
+const clientSecret = config.clientSecret;
+
+app.get('/', (req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=user:email`);
+});
+
+let token = null;
+
+app.get('/oauth-callback', async (req, res) => {
+  const body = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    code: req.query.code
+  };
+  const fetch = require("node-fetch");
+  let response = await fetch(`https://github.com/login/oauth/access_token`, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {'Content-Type': 'application/json', "Accept": "application/json"}
+  });
+  let res_ = await response.json();
+  token = res_["access_token"];
+  
+  response = await fetch("https://api.github.com/user",
+  {
+    headers: {"Accept": "application/json", "Authorization": `token ${token}`}
+  });
+  let { login } = await response.json();
+  response = await fetch("https://api.github.com/user/emails", {
+    headers: {"Accept": "application/json", "Authorization": `token ${token}`}
+  });
+  let [ email ] = await response.json();
+  
+  response = await fetch("localhost:8080/api/register", {
+    method: "POST",
+    body: JSON.stringify({
+      "name": login,
+      "email": email["email"]
+    }),
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
+  });
+  res_ = await response.json();
+  const apiKey = res_["data"]["token"];
+
+  res.cookie("apiKey", apiKey);
+  res.redirect("/customer/history");
+});
+
+
+
+
+
+
+
+
+
+
 app.use("/", routeIndex);
 // KUND
 app.use("/customer", routeCustomer);

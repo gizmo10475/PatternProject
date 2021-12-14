1a9d5dd494fa85c9d347dd68ce88a363b0ee4dba
Sigrid <sigge.jonsson@hotmail.se>
2021-12-14 15:22:35 +0100

    Removed admin routes

---
+++ src/frontend/webclientCustomer/src/adminfunc.js
@@ -0,0 +1,51 @@
+"use strict";
+const fetch = require("node-fetch");
+
+module.exports = {
+    getAllUsers: getAllUsers,
+    getUser: getUser,
+    getAllBikes: getAllBikes,
+    getAllStations: getAllStations
+};
+
+
+
+async function getAllUsers() {
+    let response = await fetch("http://localhost:8080/api/customer");
+    let {data} = await response.json();
+    return data;
+}
+
+async function getUser(id) {
+    let response = await fetch(`http://localhost:8080/api/customer/${id}`);
+    let {data} = await response.json();
+    return data;
+}
+
+
+async function getAllBikes() {
+    let response = await fetch("http://localhost:8080/api/bike");
+    let {data} = await response.json();
+    
+    let infoBikes = {};
+
+    for (var i = 0; i < data.length; i++) {
+        infoBikes[data[i].id] = [data[i].longitude, data[i].latitude]
+    }
+
+    return infoBikes;
+}
+
+
+async function getAllStations() {
+    let response = await fetch("http://localhost:8080/api/stations");
+    let {data} = await response.json();
+    
+    let infoStations = {};
+
+    for (var i = 0; i < data.length; i++) {
+        infoStations[data[i].id] = [data[i].longitude, data[i].latitude]
+    }
+
+    return infoStations;
+}

9d342e73d10c603322830d565ea27bf572ef3ab6
Wilhelm <73888475+aigr20@users.noreply.github.com>
2022-01-04 21:11:17 +0100

    Revert "Change urls for docker"

--- src/frontend/app/js/models/stations.js
+++ src/frontend/app/js/models/stations.js
@@ -4,14 +4,16 @@ import { apiKey } from "../vars.js";
 let stations = {
     mapCords: [],
     infoStations: {},
-    getAllLocations: function() {
-        return m.request({
-            method: "GET",
-            url: `http://api/api/stations`,
-            headers: {"Authorization": `Bearer ${apiKey}`}
-        }).then(function(result) {
-            stations.locations(result);
-        });
+    getAllLocations: function () {
+        return m
+            .request({
+                method: "GET",
+                url: `http://localhost:8080/api/stations`,
+                headers: { Authorization: `Bearer ${apiKey}` },
+            })
+            .then(function (result) {
+                stations.locations(result);
+            });
     },
     locations: function (info) {
         for (var i = 0; i < info.data.length; i++) {
